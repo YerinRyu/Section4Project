8916f8133bc5d02a75f39beb848b6a0622f9d459
Yerin Ryu <dmsquf728@hanyang.ac.kr>
2023-06-18 20:26:41 +0900

    version-2

--- weather/__init__.py
+++ weather/__init__.py
@@ -5,10 +5,9 @@ def create_app():
     app.config['TIMEOUT'] = 100000
     
     # blueprint
-    from weather.views import main_views, clothes_views, weather_views, dashboard
+    from weather.views import main_views, clothes_views, weather_views
     app.register_blueprint(main_views.bp)
     app.register_blueprint(clothes_views.bp)
     app.register_blueprint(weather_views.bp)
-    app.register_blueprint(dashboard.bp)
 
     return app
--- weather/views/dashboard.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, url_for, render_template
-from werkzeug.utils import redirect
-
-bp = Blueprint('dashboard', __name__, url_prefix='/')
-
-@bp.route('/dash')
-def hello_clothes():
-    return render_template('dashboard.html')
